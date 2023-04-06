876bcad95f4714ea3c02f1fe7b50a874bfd8ff0c
Bogdan <sinnlig,b@gmail.com>
2023-04-06 16:49:38 +0300

    Added file If_else.java

---
+++ src/test/java/studying1/If_else.java
@@ -0,0 +1,15 @@
+package studying1;
+
+public class If_else {
+    public static void main(String[] args) {
+//        ||  - or
+        int a =1;
+        if (a>0){
+            System.out.println( "Number is more than zero");
+        }else if(a<0){
+            System.out.println("Number is less than zero");
+        }else {
+            System.out.println( "Number is 0");
+        }
+    }
+}
